ae74ff8e87d8e68f63e12ecdfef3070c5b9d8165
Matt Forster <forster.matth@gmail.com>
2016-02-09 23:41:32 -0700

    re add test

--- test/plugins/vars.test.js
+++ test/plugins/vars.test.js
@@ -4,19 +4,19 @@ import {expect} from 'chai';
 
 describe('vars @plugin', () => {
 
-  // it('adds the vars object', (done) => {
-  //   let req = {};
-  //   let res = {};
-  //   let bound = {};
-  //
-  //   let next = () => {
-  //     expect(bound.vars).to.exist;
-  //     done();
-  //   };
-  //
-  //   vars.simple = vars.simple.bind(bound);
-  //   vars.simple(req, res, next);
-  // });
+  it('adds the vars object', (done) => {
+    let req = {};
+    let res = {};
+    let bound = {};
+
+    let next = () => {
+      expect(bound.vars).to.exist;
+      done();
+    };
+
+    vars.simple = vars.simple.bind(bound);
+    vars.simple(req, res, next);
+  });
 
 
 });
