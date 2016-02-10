33edeae4c0bc70b7807e2908467afc32d8ef5221
Matt Forster <forster.matth@gmail.com>
2016-02-09 23:00:55 -0700

    more green

---
+++ test/plugins/vars.test.js
@@ -0,0 +1,22 @@
+/* eslint no-unused-expressions:0 */
+import vars from '../../lib/plugins/vars';
+import {expect} from 'chai';
+
+describe('vars plugin', () => {
+
+  it('adds the vars object', (done) => {
+    let req = {};
+    let res = {};
+    let bound = {};
+
+    let next = () => {
+      expect(bound.vars).to.exist;
+      done();
+    };
+
+    vars.simple = vars.simple.bind(bound);
+    vars.simple(req, res, next);
+  });
+
+
+});
