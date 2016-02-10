df6094d7ef88bcd6b89df97de31e5fa8252114c2
Matt Forster <forster.matth@gmail.com>
2016-02-09 22:32:20 -0700

    sum green

---
+++ test/modules/puppyCrate.test.js
@@ -0,0 +1,90 @@
+/* eslint no-unused-expressions:0 */
+
+import PuppyCrate from '../../lib/modules/puppyCrate';
+import sinon from 'sinon';
+import {expect} from 'chai';
+import 'sinon-as-promised';
+import redis from 'redis';
+
+describe('puppyCrate', () => {
+  let sandbox;
+  let redisStub;
+  let crate;
+
+  before(() => {
+    sandbox = sinon.sandbox.create();
+
+    redisStub = {
+      on: () => false,
+      get: (key, cb) => cb(null, '[]'),
+      set: (key, value, cb) => cb(null, 'OK')
+    };
+
+    sandbox.spy(redisStub, 'get');
+    sandbox.spy(redisStub, 'set');
+    sandbox.stub(redis, 'createClient').returns(redisStub);
+  });
+
+  after(() => {
+    sandbox.restore();
+  });
+
+  // this test is infinitely useful :dog:
+  it('should add redis', () => {
+    crate = new PuppyCrate();
+
+    expect(crate.redis).to.equal(redisStub);
+  });
+
+  // so is this one
+  it('should use the url in prod', () => {
+    process.env.NODE_ENV = 'production';
+    process.env.REDIS_URL = 'localhost';
+    crate = new PuppyCrate();
+    crate.log.level(100);
+    expect(crate.redis).to.equal(redisStub);
+    expect(redis.createClient.calledWith('localhost')).to.be.true;
+  });
+
+  it('should gather the puppies', (done) => {
+    crate.gatherPuppies().then((reply) => {
+      expect(reply).to.be.an('array');
+      expect(redisStub.get.calledWith('puppies')).to.be.true;
+      done();
+    });
+
+  });
+
+  it('should gather specific puppies', (done) => {
+    crate.gatherPuppies('aarcs').then((reply) => {
+      expect(reply).to.be.an('array');
+      expect(redisStub.get.calledWith('aarcs')).to.be.true;
+      done();
+    });
+
+  });
+
+  it('should set the puppies', (done) => {
+    crate.addNew([]).then((reply) => {
+      expect(reply).to.equal('OK');
+      expect(redisStub.set.calledWith('puppies')).to.be.true;
+      done();
+    });
+  });
+
+  it('should default the key', (done) => {
+    crate.addNew().then((reply) => {
+      expect(reply).to.equal('OK');
+      expect(redisStub.set.calledWith('puppies')).to.be.true;
+      done();
+    });
+  });
+
+  it('should set specific puppies', (done) => {
+    crate.addNew('aarcs', []).then((reply) => {
+      expect(reply).to.equal('OK');
+      expect(redisStub.set.calledWith('aarcs')).to.be.true;
+      done();
+    });
+  });
+});
---
+++ test/modules/yo.test.js
@@ -0,0 +1,89 @@
+/* eslint no-unused-expressions:0 no-underscore-dangle:0 */
+
+import Yo from '../../lib/modules/yo';
+import sinon from 'sinon';
+import {expect} from 'chai';
+import 'sinon-as-promised';
+
+describe('yo', () => {
+  let sandbox;
+  let yo;
+  let requestStub;
+
+  before(() => {
+    requestStub = (options, cb) => cb(null, options);
+
+    sandbox = sinon.sandbox.create();
+  });
+
+  after(() => {
+    sandbox.restore();
+  });
+
+  it('should load the keys', (done) => {
+    process.env.YO_API = 'http://localhost';
+    process.env.YO_API_KEY = 'key';
+
+    yo = new Yo();
+    yo.request = requestStub;
+    sinon.spy(yo, 'request');
+    yo.log.level(100);
+    expect(yo.yo_url).to.equal('http://localhost');
+    expect(yo.yo_key).to.equal('key');
+    done();
+  });
+
+  describe('yo, all', () => {
+    it('should offer yoall', (done) => {
+      yo.all([]).then((reply) => {
+        expect(reply).to.be.not.ok;
+        done();
+      });
+    });
+
+    it('should process a link passed', (done) => {
+      sandbox.stub(yo, '_process').resolves(true);
+
+      yo.all('link').then((reply) => {
+        expect(yo._process.calledWith(['link']));
+        expect(reply).to.be.ok;
+        sandbox.restore();
+        done();
+      });
+    });
+
+    it('should process links passed', (done) => {
+      sandbox.stub(yo, '_process').resolves(true);
+
+      yo.all(['link', 'link']).then((reply) => {
+        expect(yo._process.calledWith(['link', 'link']));
+        expect(reply).to.be.ok;
+        sandbox.restore();
+        done();
+      });
+    });
+  });
+
+  describe('_process', () => {
+
+    it('should poll', function (done) {
+      yo.TIMEOUT = 10;
+
+      yo._process(['link1', 'link2'])
+      .then(() => {
+        expect(yo.request.callCount).to.equal(2);
+        done();
+      });
+    });
+
+    it('should reject properly', function (done) {
+      yo.request = (options, cb) => cb('fail');
+
+      yo._process(['link1', 'link2'])
+      .catch((reply) => {
+        expect(reply).to.equal('fail');
+        done();
+      });
+    });
+  });
+});
